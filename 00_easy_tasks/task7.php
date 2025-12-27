1138f936a3612a2109974d84d47d8abc6720f2d5
rub022 <rubentroncone02@gmail.com>
2025-12-27 00:49:27 -0400

    more easy tasks

---
+++ 00_easy_tasks/task7.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Task7</title>
+</head>
+<body>
+    <?php
+    // 7. Generate a random number between 1 and 100. 
+    // If it is greater than 50, print "High". Otherwise, print "Low".
+    ?>
+</body>
+</html>
---
+++ 00_easy_tasks/task8.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Task8</title>
+</head>
+<body>
+    <?php
+    // 8. Take a sentence string: 
+    // "This service is bad." Use str_replace to change "bad" to "good".
+    ?>
+</body>
+</html>
