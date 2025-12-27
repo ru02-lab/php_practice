434153ea5cb3843165ec29f5899ff3f885cdc38c
rub022 <rubentroncone02@gmail.com>
2025-12-26 20:39:58 -0400

    task6 done

--- 00_easy_tasks/task6.php
+++ 00_easy_tasks/task6.php
@@ -11,7 +11,16 @@
     // Use PHP functions to: 
     // 1. Trim the spaces. 
     // 2. Make it all lowercase.
+    $email = "   USER@gmail.com ";
+    echo "<pre>Without trim: $email</pre>";
+    echo "<br>";
 
+    $email_trimmed = trim($email);
+    echo "<pre>With trim: $email_trimmed</pre>";
+    echo "<br>";
+    
+    $email_trimmed_lowercase = strtolower($email_trimmed);
+    echo "<pre>Make it all lowercase: $email_trimmed_lowercase</pre>";
     ?>
 </body>
 </html>
